968f97e4ed08f37fadad78853449cb4cd353ea34
runburg <jack.runburg@gmail.com>
2019-03-17 20:56:58 +0000

    long run to merge

--- dm_halos.sh
+++ dm_halos.sh
@@ -3,7 +3,7 @@
 DMfile = "fe_GC_NFW"
 for fil in *.txt;
 do
-  if ["$fil" -ne "$DMfile.txt" -a "$fil" -ne "${DMfile}_nounits.txt"]
+  if ["$fil" != "$DMfile.txt" -a "$fil" != "${DMfile}_nounits.txt"]
   then
     mv "$fil" "test - $fil"
   fi
